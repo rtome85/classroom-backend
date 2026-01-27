ed2d527d711a6297592a5308a6f9fdab5b7e0459
rtome85 <rob.tome@gmail.com>
2026-01-27 15:37:52 +0000

    Implement apm with site24x7

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
+import AgentAPI from "apminsight";
+AgentAPI.config();
+
 import express from "express";
 import subjectsRouter from "./routes/subjects";
 import cors from "cors";
 import securityMiddleware from "./middleware/security";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
+import { Agent } from "node:http";
 
 const app = express();
 const PORT = 8000;
